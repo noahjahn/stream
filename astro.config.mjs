e8b1f00c9b7fa82f36000fc34403413ebe443569
Noah Jahn <najahn19@gmail.com>
2025-01-11 13:26:50 -0500

    Update name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://screenrecord.noahjahn.dev',
+  site: 'https://stream.noahjahn.dev',
 });
