21da83ba751b3757a5b02bbc11416113b0abba70
Noah Jahn <najahn19@gmail.com>
2026-05-10 16:30:44 -0500

    feat: vueify - centralize app state with vue - create shared modal component - use custom modal component instead of browser confirm - better error handling - create media component

---
+++ src/components/Media.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    },
+    srcObject: MediaStream,
+})
+
+onMounted(() => {
+    if (!props.srcObject) {
+        return
+    }
+    const video = document.getElementById(props.id) as HTMLMediaElement
+    video.srcObject = props.srcObject
+    video.play()
+})
+</script>
+
+<template>
+    <video :id="id" class="my-4 rounded-xl"></video>
+</template>
--- src/routing/public/index.ts
+++ src/routing/public/index.ts
@@ -2,6 +2,12 @@ import HomePage from '@/pages/home.vue'
 
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [{ path: '/', name: 'Home', component: HomePage }]
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomePage,
+    },
+]
 
 export default routes
